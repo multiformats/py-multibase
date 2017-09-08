9de0abdbf8121c815b828bffdbe1525bb61b1e87
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2017-09-09 00:08:52 +0400

    Bump version: 0.1.0 → 0.1.1

--- multibase/__init__.py
+++ multibase/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Dhruv Baldawa"""
 __email__ = 'dhruv@dhruvb.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .multibase import encode, decode, get_codec, is_encoded  # noqa: F401
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='py-multibase',
-    version='0.1.0',
+    version='0.1.1',
     description="Multibase implementation for Python",
     long_description=readme + '\n\n' + history,
     author="Dhruv Baldawa",
