e429a36edc50f475919dbbcbfe605937f2cdfbb0
acul71 <luca.pisani@birdo.net>
2025-12-18 01:57:52 +0100

    Fix Read the Docs build failures
    - Update deprecated Sphinx settings in docs/conf.py:
      - Replace source_suffix = '.rst' with dict format
      - Replace master_doc with root_doc
    - Add build commands to .readthedocs.yaml to generate API docs
      - Generate modules.rst and multibase.rst before build
    - Fixes missing 'modules' document warning that caused build to fail

--- docs/conf.py
+++ docs/conf.py
@@ -50,13 +50,13 @@ extensions = [
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = {'.rst': 'restructuredtext'}
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
 
-# The master toctree document.
-master_doc = 'index'
+# The root toctree document.
+root_doc = 'index'
 
 # General information about the project.
 project = u'py-multibase'
